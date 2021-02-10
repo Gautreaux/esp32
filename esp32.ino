60774d3a266bfdb0491f97556f0f8e3ec099eeb7
Spencer Gautreaux <37717452+Gautreaux@users.noreply.github.com>
2021-02-09 21:21:53 -0600

    partial work on the server board

--- esp32.ino
+++ esp32.ino
@@ -1,17 +1,28 @@
 
 #include <Arduino.h>
 #include <Wire.h>
-#include "Adafruit_MCP23017.h"
+// #include "Adafruit_MCP23017.h"
 #include <vector>
 
-#define PROBE_PIN 0
+#include <WiFi.h>
+#include "connectionSecrets.h"
 
 #define MAX_PINS 40
 
-//21, 22 removed because SDA/SCL
-const std::vector<int> gpioPins{1, 2, 3, 4, 5, 12, 13, 14, 15, 16, 17, 18, 19, 23, 25, 26, 27, 32, 33, 34, 35, 36, 39};
+#define GREEN_2 13
+#define BLUE_2 14
+#define RED_1 15
+#define RED_2 16
+#define GREEN_1 17
+#define BLUE_1 18
 
-Adafruit_MCP23017 mcp;
+//21, 22 removed because SDA/SCL
+// const std::vector<int> gpioPins{1, 2, 3, 4, 5, 12, 13, 14, 15, 16, 17, 18, 19, 23, 25, 26, 27, 32, 33, 34, 35, 36, 39};
+const std::vector<int> gpioPins{RED_1, GREEN_1, BLUE_1, RED_2, GREEN_2, BLUE_2};
+
+WiFiServer server(80); 
+
+// Adafruit_MCP23017 mcp;
 
 //scan the i2c bus and print all the addresses found
 void scanI2C()
@@ -59,20 +70,30 @@ void setup()
         pinMode(i, OUTPUT);
     }
 
-    scanI2C();
+    // scanI2C();
 
-    Serial.println("Enabling MCP23017 pins");
-    mcp.begin();
-    for (int i = 0; i < 16; i++)
-    {
-        mcp.pinMode(0, OUTPUT);
-    }
+    // Serial.println("Enabling MCP23017 pins");
+    // mcp.begin();
+    // for (int i = 0; i < 16; i++)
+    // {
+    //     mcp.pinMode(0, OUTPUT);
+    // }
 
     // pinMode(PROBE_PIN, OUTPUT);
 
     // for(int i = 0; i < MAX_PINS; i++){
     //     pinMode(i, OUTPUT);
     // }
+
+    Serial.println("Starting Webserver...");
+
+    WiFi.softAP(ssid, password);
+
+    IPAddress IP = WiFi.softAPIP();
+    Serial.print("IP Address: ");
+    Serial.println(IP);
+
+    server.begin();
 
     Serial.println("Initialization Completed.");
 }
@@ -81,30 +102,26 @@ void loop()
 {
 
     delay(1000);
-    Serial.println("ding2");
-
-    digitalWrite(PROBE_PIN, LOW);
 
     for (auto i : gpioPins)
     {
         digitalWrite(i, LOW);
     }
 
-    for (int i = 0; i < 16; i++)
-    {
-        mcp.digitalWrite(i, HIGH);
-    }
+    // for (int i = 0; i < 16; i++)
+    // {
+    //     mcp.digitalWrite(i, HIGH);
+    // }
 
     delay(1000);
-    digitalWrite(PROBE_PIN, HIGH);
 
     for (auto i : gpioPins)
     {
         digitalWrite(i, HIGH);
     }
 
-    for (int i = 0; i < 16; i++)
-    {
-        mcp.digitalWrite(i, LOW);
-    }
+    // for (int i = 0; i < 16; i++)
+    // {
+    //     mcp.digitalWrite(i, LOW);
+    // }
 }
---
+++ sampleSecrets.h
@@ -0,0 +1,5 @@
+//copy and rename to 'connectionSecrets.h'
+//  change the names as you see fit
+//  do not commit the copied file
+const char* ssid = "SampleBot";
+const char* password = "SimplePassword";
