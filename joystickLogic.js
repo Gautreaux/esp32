ca6243a7f46dee7d57d578fb3ecdcc3968ec0203
Spencer Gautreaux <37717452+Gautreaux@users.noreply.github.com>
2021-02-17 00:19:25 -0600

    joystick logic completed

--- joystickLogic.js
+++ joystickLogic.js
@@ -2,22 +2,31 @@
 
 Joystick_Wrappers = null;
 Joystick_Colliders = null;
+Joystick_Joysticks = null;
 Joystick_TouchIdentifiers = [null, null];
 Joystick_TouchStarts = [null, null];
 Joystick_TouchMoveFunctions = [null, null];
 Joystick_ResetFunctions = [null, null];
+Joystick_MaxMovement = null;
 
 function initJoysticks(){
+    let EXPECTED_LEN = 2;
     Joystick_Wrappers = document.getElementsByClassName("joystickWrapper");
 
-    if(Joystick_Wrappers.length != 2){
-        console.error("Error in joystick wrappers: expected len 2 got len " + Joystick_Wrappers.length);
+    if(Joystick_Wrappers.length != EXPECTED_LEN){
+        console.error("Error in joystick wrappers: expected len " + EXPECTED_LEN + " got len " + Joystick_Wrappers.length);
     }
 
     Joystick_Colliders = document.getElementsByClassName("joystickCollider");
 
-    if(Joystick_Colliders.length != 2){
-        console.error("Error in joystick colliders: expected len 2 got len " + Joystick_Colliders.length);
+    if(Joystick_Colliders.length != EXPECTED_LEN){
+        console.error("Error in joystick colliders: expected len " + EXPECTED_LEN + " got len " + Joystick_Colliders.length);
+    }
+
+    Joystick_Joysticks = document.getElementsByClassName("joystick");
+
+    if(Joystick_Joysticks.length != EXPECTED_LEN){
+        console.error("Error in joystick objects: expected len " + EXPECTED_LEN + " got len " + Joystick_Joysticks.length);
     }
 
     if(Joystick_Wrappers.length != Joystick_TouchIdentifiers.length){
@@ -64,12 +73,18 @@ function joystickTouchStartFactory(joystickIndex){
             Joystick_TouchIdentifiers[joystickIndex] = thisTouch.identifier;
 
             let pos = [thisTouch.clientX, thisTouch.clientY];
-            Joystick_TouchMoveFunctions[joystickIndex] = pos;
+            Joystick_TouchStarts[joystickIndex] = pos;
 
             console.log("New touch on joystick " + joystickIndex + ": " + pos);
 
+            Joystick_Colliders[joystickIndex].addEventListener("touchmove", Joystick_TouchMoveFunctions[joystickIndex]);
+
             //TODO - code for the movement size and things
-            //setting max movement ssize
+            //setting max movement size
+
+            let p = Joystick_Colliders[joystickIndex].parentElement.parentElement.clientHeight;
+            let j = Joystick_Colliders[joystickIndex].clientHeight;
+            Joystick_MaxMovement = (.5*(p-j));
         }
     }
 }
@@ -84,28 +99,6 @@ function joystickTouchEndFactory(joystickIndex){
             return;
         }
 
-        //will be empty on the thingy
-        console.log(event.targetTouches);
-
-        var thisTouch = null;
-
-        for(i = 0; i < event.changedTouches.length; i++)
-        {
-            if(event.changedTouches[i].identifier = Joystick_TouchIdentifiers[joystickIndex])
-            {
-                thisTouch = event.changedTouches[i];
-                break;
-            }
-        }
-        
-        if(thisTouch === null){
-            console.error("On release of joystick " + joystickIndex + " could not find relevant touch start");
-            Joystick_ResetFunctions[joystickIndex]();
-            return;
-        }
-
-        //TODO - log info and things;
-
         Joystick_ResetFunctions[joystickIndex]();
     }
 }
@@ -114,11 +107,58 @@ function joystickTouchMoveFactory(joystickIndex){
     return function(event){
         event.preventDefault();
         console.log("Joystick-move index: " + joystickIndex);
+
+        var thisTouch = null;
+
+        for(i = 0; i < event.targetTouches.length; i++){
+            if(event.targetTouches[i].identifier == Joystick_TouchIdentifiers[joystickIndex]){
+                thisTouch = event.targetTouches[i];
+                break;
+            }
+        }
+
+        if(thisTouch === null)
+        {
+            //should be unreachable
+            console.error("Lost this touch from touchpoint set.")
+            Joystick_ResetFunctions[joystickIndex]();
+            return;
+        }
+
+        var thisPos = [thisTouch.clientX, thisTouch.clientY];
+        // console.log(thisPos);
+
+        let thisVector = [
+            ((thisPos[0]-Joystick_TouchStarts[joystickIndex][0])/Joystick_MaxMovement),
+            ((thisPos[1]-Joystick_TouchStarts[joystickIndex][1])/Joystick_MaxMovement),
+        ];
+
+        let vectorMagnitude = Math.sqrt(Math.pow(thisVector[0], 2) + Math.pow(thisVector[1], 2));
+        if(vectorMagnitude > 1){
+            thisVector[0] /= vectorMagnitude;
+            thisVector[1] /= vectorMagnitude;
+        }
+
+        console.log(thisVector);
+
+        let j = Joystick_Joysticks[joystickIndex];
+        j.style.left = String(thisVector[0]*Joystick_MaxMovement);
+        j.style.top = String(thisVector[1]*Joystick_MaxMovement);
     }
 }
 
 function joystickResetFactory(joystickIndex){
     return function(){
         console.log("Joystick-reset index: " + joystickIndex);
+
+        Joystick_TouchIdentifiers[joystickIndex] = null;
+        Joystick_TouchStarts[joystickIndex] = null;
+        
+        Joystick_Colliders[joystickIndex].removeEventListener("touchmove", Joystick_TouchMoveFunctions[joystickIndex]);
+
+        Joystick_Joysticks[joystickIndex].style.left = "0";
+        Joystick_Joysticks[joystickIndex].style.top = "0";
+
+        //TODO - send to webserver
     }
 }
